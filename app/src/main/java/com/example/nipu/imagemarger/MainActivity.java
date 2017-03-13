83487a50f4dd3db6609c2139aef1e01ce8553262
nipu <fuad031@gmail.com>
2017-03-13 22:02:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nipu/imagemarger/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nipu.imagemarger;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Gallery;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
